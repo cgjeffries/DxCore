14d8258ed0788c45834cf8e1455a575bbd81cff0
JoshZimmerling <84738283+JoshZimmerling@users.noreply.github.com>
2022-11-09 19:47:47 -0700

    Adding in the header file into DxCore

---
+++ megaavr/cores/dxcore/ADC.h
@@ -0,0 +1,51 @@
+//////////////////////////////////////////////////////////////
+//                     General ADC API                      //
+//////////////////////////////////////////////////////////////
+
+/*
+Structure used for the ADC configuration
+*/
+typedef struct ADCConfig_struct {
+    uint8_t convMode;    // 0 = single-ended, 1 = differential
+    uint8_t muxNeg;      // Differential mode negative pin
+    uint8_t resolution;  // ADC bit resolution
+    uint8_t prescaler;   // ADC clock prescaler
+    uint8_t sampleNum;   // Number of samples to accumulate - should define constants for these
+    uint8_t sampleDelay; // Delay in ADC clock cycles between samples during accumulation
+    uint8_t sampleLen;   // ADC sampling time in ADC clock cycles
+} ADCConfig;
+
+/*
+Sets the registers to fit the passed in configuration
+*/
+void analogReadConfig(ADCConfig config)
+
+/*
+Specify an accumulation amount
+*/
+void analogReadSampleNum(uint8_t numSamples);
+
+/*
+Specify the delay between samples
+*/
+void analogReadSampleDelay(uint8_t delay)
+
+/*
+Specify the length of each sample
+*/
+void analogReadSampleLen(uint8_t len)
+
+/*
+Set the prescalaer
+*/
+void analogReadPrescaler(uint8_t prescaler);
+
+/*
+Enables single-ended mode
+*/
+void analogReadEnableSingleEnded();
+
+/*
+Enables differential mode
+*/
+void analogReadEnableDifferential(uint8_t negPin);
