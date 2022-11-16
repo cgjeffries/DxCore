f2cd4914d3e4214fd5accef6d35bc1b57782f595
Charles Jeffries <sazrocks6@gmail.com>
2022-11-16 06:56:06 -0700

    refromat and add docs

--- megaavr/cores/dxcore/low_power.cpp
+++ megaavr/cores/dxcore/low_power.cpp
@@ -4,46 +4,56 @@
 
 // Helper functions
 
-void wake_using_RTC(){
-    /* Initialize RTC: */
+//function to wait for the RTC register to sync with main clock. RTC can be run on different clock so waiting for this sync is good and prevents odd behavior from appearing.
+void RTC_sync(){
   while (RTC.STATUS > 0)
   {
-    ;                                   /* Wait for all register to be synchronized */
+    ;                                   /* Wait for all registers to be synchronized */
   }
+}
+
+//configure the RTC to enabled and set the overflow interrupt to enabled
+void wake_using_RTC(){
+    /* Initialize RTC: */
+  RTC_sync();
   RTC.CLKSEL = RTC_CLKSEL_OSC1K_gc;  /* 1.024 kHz from OSC32K */
   RTC.INTCTRL = RTC_OVF_bm;					// enable overflow interrupt
   RTC.CTRLA = 0 | 0x80 | 1;		// set enabled in standby and enable RTC
 }
 
+//function that scales millis to RTC ticks based on 1024 ticks/sec
 void set_RTC_per_time(uint32_t millis){
-  while (RTC.STATUS > 0)
-  {
-    ;                                   /* Wait for all register to be synchronized */
-  }
+  RTC_sync();
   RTC.PER = (millis * 1024) / 1000;
 }
 
+//Is this something that we should be worried about? What if the user wants to define their own ISR?
+//using #define guards for this. If the user needs this ISR for themselves they can #undef the RTC_WAKE_ISR.
+#define RTC_WAKE_ISR
+#ifdef RTC_WAKE_ISR
 ISR(RTC_CNT_vect)
 {
   RTC.INTFLAGS = RTC_OVF_bm;          /* Clear interrupt flag by writing '1' (required) */
 }
+#endif
 
+//disable the RTC, that way it won't keep generating interrupts
 void disable_RTC(){
-  while (RTC.STATUS > 0)
-  {
-    ;                                   /* Wait for all register to be synchronized */
-  }
+  RTC_sync();
   RTC.CTRLA = 0;					// disable RTC
 }
 
+//temp function, will be replaced with call to API-adhered function
 void set_sleep_mode_standby(){
   SLPCTRL.CTRLA = SLPCTRL_SMODE_t::SLPCTRL_SMODE_STDBY_gc; //select power down state
 }
 
+//temp function, will be replaced with call to API-adhered function
 void enable_sleep_mode(){
   SLPCTRL.CTRLA |= 1; //enable sleep
 }
 
+//temp function, will be replaced with call to API-adhered function
 void enter_sleep_mode(){
   asm("sleep \n");
 }
