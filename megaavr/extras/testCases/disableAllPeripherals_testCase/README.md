ca0caa824ba075f61ada087a6c1a1e9f865a4ec9
amtrang <61100945+amtrang@users.noreply.github.com>
2022-12-02 09:41:54 -0700

    Included Results
    Included an example results for the disableAllPeripheralsDuringSleep() function. This test case should work for testing that function.

---
+++ megaavr/extras/testCases/disableAllPeripherals_testCase/README.md
@@ -0,0 +1,3 @@
+### Note a couple of things before using this test case
+- It's unable to use Serial1 for printing statements on the Serial monitor
+- Press the user switch on the board to exit Standby mode and move on to Idle mode
--- megaavr/extras/testCases/disableAllPeripherals_testCase/disableAllPeripherals_testCase.ino
+++ megaavr/extras/testCases/disableAllPeripherals_testCase/disableAllPeripherals_testCase.ino
@@ -2,33 +2,24 @@ const int userSwitchPin = 21; // PC7
 
 // Use a voltmeter or power debugger to check the power draw
 void setup() {
-  Serial1.begin(38400);
   pinMode(userSwitchPin, INPUT_PULLUP); // PC7 has an internal pull-up resistor
   attachInterrupt(digitalPinToInterrupt(userSwitchPin), wake_up, CHANGE);
 }
 
 void loop() {
-  Serial1.println("Disabled Peripherals");
+  // Peripherals Disabled
   disableAllPeripheralsDuringSleep();
-  Serial1.println("Entering Standby Mode");
-  sleepSimple(SLEEP_MODE_STANDBY, true);
-  delaySleep(2000);
-  Serial1.println("Entering Idle Mode");
+  sleepSimple(SLEEP_MODE_STANDBY, true); // Press user switch here
   sleepSimple(SLEEP_MODE_IDLE, true);
-  delay(2000);
+  delay(3000);
 
-  Serial1.println("Enabled Peripherals");
+  // Peripherals Enabled
   enableAllPeripheralsDuringSleep();
-  Serial1.println("Entering Standby Mode");
-  sleepSimple(SLEEP_MODE_STANDBY, true);
-  delaySleep(2000);
-  Serial1.println("Entering Idle Mode");
+  sleepSimple(SLEEP_MODE_STANDBY, true); // Press user switch here
   sleepSimple(SLEEP_MODE_IDLE, true);
-  delay(2000);
-
-  Serial1.println(" - Looped - ");
+  delay(3000);
 }
 
 void wake_up() {
-  Serial1.println("[Woken up]");
+
 }
