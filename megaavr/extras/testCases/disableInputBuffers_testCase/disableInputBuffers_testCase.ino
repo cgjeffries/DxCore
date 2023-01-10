76b45507aca244a24c98435979bba481909922ec
amtrang <61100945+amtrang@users.noreply.github.com>
2023-01-10 14:14:26 -0700

    Test Case first draft
    Created the first draft to test disableInputBuffers & enableInputBuffers. Also included a picture showing the results. When input buffers are disabled, power draw is about 6.4 mA. When they're enabled, power draw is about 6.0 mA.

---
+++ megaavr/extras/testCases/disableInputBuffers_testCase/disableInputBuffers_testCase.ino
@@ -0,0 +1,28 @@
+const int ledPin = 20; // PC6
+
+// Use a voltmeter or power debugger to check the power draw
+void setup() {
+  Serial1.begin(38400);
+}
+
+void loop() {
+  // Disable Input Buffers
+  disableInputBuffers();
+  Serial1.println("DISABLE : IDLE");
+  sleepSimple(SLEEP_MODE_IDLE, true);
+  delay(2000);
+  Serial1.println("DISABLE : ACTIVE");
+  disableSleepMode();
+  delay(2000);
+
+  // Enable Input Buffers
+  enableInputBuffers();
+  Serial1.println("ENABLE : IDLE");
+  sleepSimple(SLEEP_MODE_IDLE, true);
+  delay(2000);
+  Serial1.println("ENABLE : ACTIVE");
+  disableSleepMode();
+  delay(2000);
+
+  Serial1.println(" - Looped - ");
+}
