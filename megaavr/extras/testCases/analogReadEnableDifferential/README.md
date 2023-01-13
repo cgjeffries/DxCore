317f612147bdcf7efb54ae99df474023ae91ac13
Matt Cummings <mpc.ender@gmail.com>
2023-01-12 19:44:12 -0700

    Completed test case, unexpected output from analogRead

--- megaavr/extras/testCases/analogReadEnableDifferential/README.md
+++ megaavr/extras/testCases/analogReadEnableDifferential/README.md
@@ -3,13 +3,21 @@ This test case demonstrates changing the usage of analogReadEnableDifferential
 and demonstrates the output to serial monitor.
 
 ## Instructions
-1. Upload the `analogReadEnableDifferential.ino` sketch to your AVRDA board.
-2. Open a serial monitor to view the recorded readings.
+1. Connect 5v (VTG) to Positive input (PD0)
+2. Connect signal generator with DC input to negative pin (PD1)
+3. Upload the `analogReadEnableDifferential.ino` sketch to your AVRDA board.
+4. Open a serial monitor to view the recorded readings.
 
 ## Expected Output
+Single Ended Mode
  - `(Default)` output will be 0 indicating the ADC is in single-ended mode.
+ - Average sample output is ~5v
+Differential Mode
  - `(Enabled)` output should be 1 indicating the analogReadEnableDifferential function 
 executed sucessfully and enabled differential mode on the ADC.
+ - Average sample output is ~5v minus negative pin input
 
 ## Results
-...
+Result sample output demonstrates the single ended mode outputting 5v and the 
+analogReadEnableDifferential function sucessfully enabling differential mode and 
+outputting the differential voltage (positive minus negative).
--- megaavr/extras/testCases/analogReadEnableDifferential/analogReadEnableDifferential.ino
+++ megaavr/extras/testCases/analogReadEnableDifferential/analogReadEnableDifferential.ino
@@ -3,7 +3,10 @@
 */
 
 #define da_Serial Serial1
-#define neg_pin A1 // select the input pin NEGATIVE (PD1) 
+
+int POS_PIN = A0;   // select the input pin POSITIVE (PD0)
+int NEG_PIN = A1;   // select the input pin NEGATIVE (PD1 - Signal Noise Generator) 
+int ledPin = 20;      // select the pin for the LED
 
 
 void setup() {
@@ -15,22 +18,42 @@ void loop() {
     ADCConfig defaultConfig = ADCConfig_default;
     analogReadConfig(defaultConfig);
 
-    printConvMode("convMode (Default)", getAnalogReadConfig());
+    sample(getAnalogReadConfig().convMode);
 
-    da_Serial.println("get config after updating to arbitrary config:");
-    analogReadEnableDifferential(neg_pin);
-	
-    printConvMode("convMode (Enabled)", getAnalogReadConfig());
-	
-	#TODO: Read some input to test neg_pin input
+    analogReadEnableDifferential(NEG_PIN);
+
+    sample(getAnalogReadConfig().convMode);
 
     printSeperator();
 
     delay(5000);
   }
 
-void printConvMode(String msg, ADCConfig config) {
-  da_Serial.printf("%s: %d", msg, config.convMode);
+#define SAMPLE_NUMBER 128
+
+void sample(int convMode) {
+  int adc_value, sample_index;
+
+  digitalWrite(ledPin, HIGH);
+  da_Serial.println("begin:");
+  for (sample_index = 0; sample_index < SAMPLE_NUMBER; sample_index++) {
+    adc_value = analogRead(POS_PIN);
+    /*
+	  if (convMode == 0) {
+      adc_value = analogReadEnh(POS_PIN, 12, 0);
+    } else {
+      adc_value = analogReadDiff(POS_PIN, NEG_PIN, 12, 0);
+    }
+    */
+	
+    da_Serial.print(sample_index);
+    da_Serial.print(" ");
+    da_Serial.println(adc_value);
+    da_Serial.print(" ");
+  }
+
+  da_Serial.println("end:\n\n");
+  digitalWrite(ledPin, LOW);  
 }
 
 void printSeperator() {
