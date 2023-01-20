da14694576c5d011782521d1c678693715c34410
JoshZimmerling <84738283+JoshZimmerling@users.noreply.github.com>
2023-01-20 16:16:38 -0700

    Starting a ino file to test periodic noise

---
+++ megaavr/extras/testCases/periodicNoise/README.md
@@ -0,0 +1,10 @@
+# Periodic Noise Cancellation Test Case
+This test case demonstrates the changes in output with periodic noise input. This test case 
+will test how analogRead output, with a change in number of samples recorded, is affected while 
+there is periodic noise in the background
+
+## Instructions
+3. Upload the `periodicNoise.ino` sketch to your AVRDA board.
+4. Using the noise generator, set the signal source to DC enable.
+5. Enable periodic noise on the noise generator
+6. Open a serial monitor to view the recorded readings.
---
+++ megaavr/extras/testCases/periodicNoise/periodicNoise.ino
@@ -0,0 +1,48 @@
+/*
+  Program testing periodic noise
+*/
+
+#define da_Serial Serial1
+
+void setup() {
+  da_Serial.begin(38400);     //Data transmission rate
+
+  //ADC Set-up
+  analogReference(0x05);      //Setting reference voltage
+}
+
+void loop() {
+
+  da_Serial.println("4 samples being accumulated");
+
+  for(int i = 0; i < 30; i++){
+    analogReadSampleNum(4);
+    int adc_value = analogRead(A0);
+    while((ADC0.COMMAND & 0b00000001) > 0){} //Wait for conversion to finish
+    da_Serial.println(adc_value);
+  }
+
+  delay(500);
+
+
+  da_Serial.println("16 samples being accumulated");
+
+  for(int i = 0; i < 30; i++){
+    analogReadSampleNum(16);
+    int adc_value = analogRead(A0);
+    while((ADC0.COMMAND & 0b00000001) > 0){} //Wait for conversion to finish
+    da_Serial.println(adc_value);
+  }
+
+  delay(500);
+}
+
+void printSample(uint8_t accumValue) {
+  analogReadSampleNum(accumValue);
+  int adc_value = analogRead(A0);
+  while((ADC0.COMMAND & 0b00000001) > 0){} //Wait for conversion to finish
+  da_Serial.print("Result with ");
+  da_Serial.print(accumValue);
+  da_Serial.print(" samples accumulated: ");
+  da_Serial.println(adc_value);
+}
