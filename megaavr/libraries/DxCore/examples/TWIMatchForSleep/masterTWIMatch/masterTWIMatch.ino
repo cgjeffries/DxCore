ddc00fc57d781502973b8509f42189e1508f38da
amtrang <61100945+amtrang@users.noreply.github.com>
2023-01-19 22:33:23 -0700

    TWIMatch: Master & Slave setup
    Setup the .ino files for the master and slave devices so that the master should wake up the slave with a TWI address match. Will need to do some initial tests for these files soon.

--- megaavr/libraries/DxCore/examples/TWIMatchForSleep/masterTWIMatch/masterTWIMatch.ino
+++ megaavr/libraries/DxCore/examples/TWIMatchForSleep/masterTWIMatch/masterTWIMatch.ino
@@ -1,13 +1,58 @@
+// Used master_write.ino from libraries\Wire\examples\master_write
+// in the DxCore package for reference
+
 #include <Wire.h>
 
+#define TestSerial Serial1
+
+uint8_t twiAddress = 0x08;
+bool useInternalPullups = true;
+
+char serialInput[32];
+int8_t serialInputLength = 0;
+
+int delayTime = 1000;
+
 void setup() {
-  Serial1.begin(38400);
+  TestSerial.begin(38400);
+  wakeWithTWIMatch(useInternalPullups);
 }
 
 void loop() {
-  
+  delay(delayTime);
+  TestSerial.write(0x11);
+  if (TestSerial.available() > 0) {
+    readFromSerial();
+    if (serialInputLength > 0) {
+      sendDataWire(twiAddress);
+    }
+    serialInputLength = 0;
+  }
 }
 
-void wakeWithTWIMatch(uint8_t address) {
-  
+void wakeWithTWIMatch(bool useInternalPullups) {
+  if (useInternalPullups) {
+    Wire.usePullups(); // Not recommended, not as reliable as exteral pullups
+  }
+  Wire.begin();
+}
+
+void readFromSerial() {
+  while (serialInputLength <= 30) {
+    while (TestSerial.available() == 0); // Wait until all bytes are taken in
+    char character = TestSerial.read();
+    if (character == '\r' || character == '\n') {
+      break;
+    }
+    serialInput[serialInputLength++] = character;
+  }
+}
+
+void sendDataWire(uint8_t twiAddress) {
+  Wire.beginTransmission(twiAddress);
+  for (int inputIndex = 0; inputIndex < serialInputLength; inputIndex++) {
+    Wire.write(serialInput[inputIndex]);
+  }
+  Wire.write("\r\n");
+  Wire.endTransmission();
 }
--- megaavr/libraries/DxCore/examples/TWIMatchForSleep/slaveTWIMatch/slaveTWIMatch.ino
+++ megaavr/libraries/DxCore/examples/TWIMatchForSleep/slaveTWIMatch/slaveTWIMatch.ino
@@ -1,13 +1,41 @@
+// Used slave_read.ino from libraries\Wire\examples\slave_read
+// in the DxCore package for reference
+
 #include <Wire.h>
 
+#define TestSerial Serial1
+
+SLPCTRL_SMODE_t sleepMode = SLEEP_MODE_STANDBY;
+uint8_t twiAddress = 0x08;
+bool useInternalPullups = true;
+bool flushSerial = true;
+
+uint8_t delayTime = 100;
+
+// Addresses 0x08 to 0x6F are available, the rest are for special cases found here:
+// https://github.com/cgjeffries/DxCore/tree/master/megaavr/libraries/Wire
 void setup() {
-  Serial1.begin(38400);
+  TestSerial.begin(38400);
+  sleepUntilTWIMatch(twiAddress, useInternalPullups, SLEEP_MODE_STANDBY, flushSerial);
 }
 
 void loop() {
-  
+  delay(delayTime);
 }
 
-void sleepUntilTWIMatch(uint8_t address) {
-  
+// MALS-115
+void sleepUntilTWIMatch(uint8_t twiAddress, bool useInternalPullups, SLPCTRL_SMODE_t sleepMode, bool flushSerial) {
+  if (useInternalPullups) {
+    Wire.usePullups(); // Not recommended, not as reliable as exteral pullups
+  }
+  Wire.begin(twiAddress);
+  Wire.onReceive(receiveFromMaster);
+  sleepSimple(sleepMode, flushSerial);
+}
+
+// This should wake the microcontroller when it's called as an event by the Wire
+void receiveFromMaster(int16_t numMasterBytes) {
+  for (int bitPos = 0; bitPos < numMasterBytes; bitPos++) {
+    TestSerial.write(Wire.read());
+  }
 }
