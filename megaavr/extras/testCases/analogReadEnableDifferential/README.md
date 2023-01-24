683cef66d7a746a714dd7810d5dd7145cf27a564
Matt Cummings <mpc.ender@gmail.com>
2023-01-24 16:08:19 -0700

    Revision of test case to fix output & allow boundary analysis

--- megaavr/extras/testCases/analogReadEnableDifferential/README.md
+++ megaavr/extras/testCases/analogReadEnableDifferential/README.md
@@ -2,23 +2,18 @@
 This test case demonstrates changing the usage of analogReadEnableDifferential 
 and demonstrates the output to serial monitor.
 
-## Instructions
-1. Connect 5v (VTG) to Positive input (PD0)
-2. Connect signal generator with DC input to negative pin (PD1)
-3. Upload the `analogReadEnableDifferential.ino` sketch to your AVRDA board.
-4. Open a serial monitor to view the recorded readings.
-
-## Expected Output
-Single Ended Mode
- - `Single Ended Mode: (ConvMode = #)` output will be 0 indicating the ADC is 
- in single-ended mode.
- - Average sample output is ~5v
-Differential Mode
- - `"Differential Mode: (ConvMode = #)` output should be 1 indicating the analogReadEnableDifferential function executed sucessfully and enabled 
- differential mode on the ADC.
- - Average sample output is ~5v minus negative pin input
-
-## Results
-Result sample output demonstrates the output variations in modifying the postive 
-and negative input volatges and toggling single ended or differential mode. The
-results trends are consistent with expected values.
+# Notes
+DxCore Refrence Voltage modes at `megaavr\extras\Ref_Analog.md`
+| AVR Dx/Ex-series (all)                  | Voltage | Minimum Vdd | Mode # |
+|-----------------------------------------|---------|-------------|--------|
+| `VDD` (default 1.024V)                  | Vcc/Vdd |             |      5 |
+| `INTERNAL1V024`                         | 1.024 V |      2.5* V |      0 |
+| `INTERNAL2V048`                         | 2.048 V |      2.5  V |      1 |
+| `INTERNAL4V096`                         | 4.096 V |      4.55 V |      2 |
+| `INTERNAL2V500`                         | 2.500 V |      2.7  V |      3 |
+| `INTERNAL4V1` (alias of INTERNAL4V096)  | 4.006 V |      4.55 V |      2 |
+| `EXTERNAL`                              | VREF pin|        VREF |      6 |
+ * `EXTERNAL` -> VREF pin -> pin (PD7)
+ * VREF - Voltage Reference (pg 198 Data Sheet)
+ 
+ The DAC Output Can *ONLY* Be Used as Input to the ADC Positive Input. (pg 508)
--- megaavr/extras/testCases/analogReadEnableDifferential/analogReadEnableDifferential.ino
+++ megaavr/extras/testCases/analogReadEnableDifferential/analogReadEnableDifferential.ino
@@ -1,49 +1,95 @@
 /*
   Program demonstrating getAnalogEnableDifferential
 */
-
+#include <Comparator.h>
 #define da_Serial Serial1
 
-int POS_PIN = A0;   // select the input pin POSITIVE (PD0)
-int NEG_PIN = A1;   // select the input pin NEGATIVE (PD1 - Signal Noise Generator) 
+int POS_PIN = ADC_DACREF0; //A0; // Positive pin (DAC or swap `A0;` to use external pin PD0) 
+int NEG_PIN = A1; // select the input pin NEGATIVE (PD1 -> pins-arduino.h )
+
+// Calculate Expected Value
+double VAINP = 1.024; // POSITIVE (DAC) (Set reference in setDAC() for expected value)
+double VAINN = 1.0; // NEGATIVE (external pin PD0)
+double VADCREF = 1.024; // VREF;  (Set VREF in setVREF() for expected value)
 
 void setup() {
   da_Serial.begin(38400); // Baud rate
 }
 
 void loop() {  
-    ADCConfig defaultConfig = ADCConfig_default;
-    analogReadConfig(defaultConfig);
 
-    da_Serial.printf("Single Ended Mode: (ConvMode = %d)", getAnalogReadConfig().convMode);
-    da_Serial.println();
-    sample(getAnalogReadConfig().convMode);
-
-    analogReadEnableDifferential(NEG_PIN);
-    da_Serial.printf("Differential Mode: (ConvMode = %d)", getAnalogReadConfig().convMode);
-    da_Serial.println();
-    sample(getAnalogReadConfig().convMode);
-
+    // Set DAC -> POSITIVE -> Magnitude of variance
+    setDAC();
+    // Set VREF -> Magnitude of variance
+    setVREF();
+    
+    sample();
+    printExpectedOutput();
     printSeperator();
 
     delay(5000);
   }
 
+void setDAC() {
+	// DxCore Refrence Voltage modes at `megaavr\extras\Ref_Analog.md`
+    /*| AVR Dx/Ex-series (all)                  | Voltage | Minimum Vdd | Mode # |
+      |-----------------------------------------|---------|-------------|--------|
+      | `VDD` (default)                         | Vcc/Vdd |             |      5 |
+      | `INTERNAL1V024`                         | 1.024 V |      2.5* V |      0 |
+      | `INTERNAL2V048`                         | 2.048 V |      2.5  V |      1 |
+      | `INTERNAL4V096`                         | 4.096 V |      4.55 V |      2 |
+      | `INTERNAL2V500`                         | 2.500 V |      2.7  V |      3 |
+      | `INTERNAL4V1` (alias of INTERNAL4V096)  | 4.006 V |      4.55 V |      2 |
+      | `EXTERNAL` (DA pin PD7)                 | VREF pin|        VREF |      6 |
+        */
+    DACReference(INTERNAL1V024)
+}
+
+
+void setVREF(){
+  // DxCore Refrence Voltage modes at `megaavr\extras\Ref_Analog.md`
+    /*| AVR Dx/Ex-series (all)                  | Voltage | Minimum Vdd | Mode # |
+      |-----------------------------------------|---------|-------------|--------|
+      | `VDD` (default)                         | Vcc/Vdd |             |      5 |
+      | `INTERNAL1V024`                         | 1.024 V |      2.5* V |      0 |
+      | `INTERNAL2V048`                         | 2.048 V |      2.5  V |      1 |
+      | `INTERNAL4V096`                         | 4.096 V |      4.55 V |      2 |
+      | `INTERNAL2V500`                         | 2.500 V |      2.7  V |      3 |
+      | `INTERNAL4V1` (alias of INTERNAL4V096)  | 4.006 V |      4.55 V |      2 |
+      | `EXTERNAL` (DA pin PD7)                 | VREF pin|        VREF |      6 |
+        */
+    // VREF mode (magnitude)
+    analogReference(INTERNAL1V024);
+}
+
+
 #define SAMPLE_NUMBER 128
-
-void sample(int convMode) {
+void sample() {
   int adc_value, sample_index;
+  double avg = 0;
 
-  da_Serial.println("begin:");
   for (sample_index = 0; sample_index < SAMPLE_NUMBER; sample_index++) {
     adc_value = analogRead(POS_PIN);
-	
-    da_Serial.printf(" %d\t%d", sample_index, adc_value/4);
-    da_Serial.println();
+    avg = avg + adc_value;
   }
-
-  da_Serial.println("end:\n\n");
+  int out = (avg/sample_index);
+  da_Serial.printf("\tACTUAL ADC value  : %d", out);
+  da_Serial.println();
+  
 }
+
+void printExpectedOutput() {
+  // Voltage calculations [-512,512] (pg 484)
+    double expectedOutput = -1;
+    if (getAnalogReadConfig().resolution == 10) {
+      expectedOutput = (((VAINP - VAINN) / VADCREF) * 512); // [-512,512]
+    } else {
+      expectedOutput = (((VAINP - VAINN) / VADCREF) * 2048); // [-2048,2047]
+    }
+    da_Serial.printf("\tEXPECTED ADC value: ");
+    da_Serial.println(expectedOutput);
+}
+
 
 void printSeperator() {
   da_Serial.println("-------------------------------------------------");
