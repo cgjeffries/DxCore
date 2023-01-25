38d7578552e969ecda54f7a65713774f5d55fb42
amtrang <61100945+amtrang@users.noreply.github.com>
2023-01-25 15:05:18 -0700

    MALS-110 Finished
    Finished the test case for MALS-110. Use the DA board as the slave and the XPlained Mini board as the master. The two .ino files will run automatically. To validate this for MALS 130, use a power debugger to draw the graph and included it in a README.md file. Also include any additional comments and changes of the scripts to the README file.

---
+++ megaavr/extras/testCases/sleepUntilTWIMatch_testCase/slaveTWIMatch_testCase/slaveTWIMatch_testCase.ino
@@ -0,0 +1,73 @@
+// Used slave_read.ino from libraries\Wire\examples\slave_read
+// in the DxCore package by MX682X for reference
+
+// Slave is AVR128DA48 Curiosity Nano board
+// Recommended baud for Serial1 is 115200
+#include <Wire.h>
+
+#define SlaveSerial Serial1
+
+unsigned long serialBaud = 115200;
+
+uint8_t sleepModeNum = 2;
+SLPCTRL_SMODE_t sleepMode = SLEEP_MODE_PWR_DOWN;
+uint8_t twiAddress = 0x08;
+bool useInternalPullups = false;
+bool flushSerial = true;
+
+unsigned long delayTime = 1000;
+bool isAwake = false;
+
+// Addresses 0x08 to 0x6F are available, the rest are for special cases found here:
+// https://github.com/cgjeffries/DxCore/tree/master/megaavr/libraries/Wire
+void setup() {
+  SlaveSerial.begin(serialBaud);
+  pickSleepMode(sleepModeNum);
+  sleepUntilTWIMatch(twiAddress, useInternalPullups, sleepMode, flushSerial);
+}
+
+void loop() {
+  delay(delayTime / 10);
+  if (isAwake) {
+    delay(delayTime);
+    isAwake = false;
+    sleepSimple(sleepMode, flushSerial);
+  }
+}
+
+void sleepUntilTWIMatch(uint8_t twiAddress, bool useInternalPullups, SLPCTRL_SMODE_t sleepMode, bool flushSerial) {
+  if (useInternalPullups) {
+    Wire.usePullups(); // Not recommended, not as reliable as exteral pullup resistors
+  }
+  Wire.begin(twiAddress);
+  Wire.onReceive(receiveFromMaster);
+  sleepSimple(sleepMode, flushSerial);
+}
+
+// This should wake the microcontroller when it's called as an event by the Wire
+void receiveFromMaster(int16_t numMasterBytes) {
+  SlaveSerial.write("Received from Master: ");
+  for (int bitPos = 0; bitPos < numMasterBytes; bitPos++) {
+    SlaveSerial.write(Wire.read());
+  }
+  if (++sleepModeNum > 2) {
+    sleepModeNum = 0;
+  }
+  pickSleepMode(sleepModeNum);
+  isAwake = true;
+}
+
+void pickSleepMode(uint8_t sleepModeNum) {
+  if (sleepModeNum == 0) {
+    SlaveSerial.write("Idle picked\n");
+    sleepMode = SLEEP_MODE_IDLE;
+  }
+  else if (sleepModeNum == 1) {
+    SlaveSerial.write("Standby picked\n");
+    sleepMode = SLEEP_MODE_STANDBY;
+  }
+  else if (sleepModeNum == 2) {
+    SlaveSerial.write("Power down picked\n");
+    sleepMode = SLEEP_MODE_PWR_DOWN;
+  }
+}
